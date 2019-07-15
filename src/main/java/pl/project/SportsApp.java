f6d7a93cf7bf000011f07b1f488a2a678db04c2b
Michał Chyb <michalchyb1@gmail.com>
2019-07-15 18:44:16 +0200

    initial commit

---
+++ src/main/java/pl/project/SportsApp.java
@@ -0,0 +1,13 @@
+package pl.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsApp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SportsApp.class, args);
+	}
+}
