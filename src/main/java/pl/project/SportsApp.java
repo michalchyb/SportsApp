0af4d1103438361d3ce4283a9eb8199387c2ef5d
Michał Chyb <michalchyb1@gmail.com>
2019-07-16 19:18:34 +0200

    added log4j

--- src/main/java/pl/project/SportsApp.java
+++ src/main/java/pl/project/SportsApp.java
@@ -1,13 +1,16 @@
 package pl.project;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SportsApp {
+    private static final Logger logger = LogManager.getLogger(SportsApp.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(SportsApp.class, args);
-	}
+        SpringApplication.run(SportsApp.class, args);
+    }
 }
