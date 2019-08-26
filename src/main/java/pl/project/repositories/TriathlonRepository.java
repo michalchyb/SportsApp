9a79335441535e2119fe55d8c47c328f36ce0c5c
Michał Chyb <michalchyb1@gmail.com>
2019-08-26 17:46:36 +0200

    added query to triathlon repository

--- src/main/java/pl/project/repositories/TriathlonRepository.java
+++ src/main/java/pl/project/repositories/TriathlonRepository.java
@@ -1,6 +1,7 @@
 package pl.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.project.models.Triathlon;
 
 import java.util.List;
@@ -14,5 +15,18 @@ public interface TriathlonRepository extends JpaRepository<Triathlon, Long> {
     List<Triathlon> findTriathlonsByDistance(Double distance);
 
     void deleteByNameRun(String runName);
+
+    @Query(value = "SELECT *  from run t  WHERE t.distance = 28.25 limit 5",
+            nativeQuery = true)
+    List<Triathlon> findLast2825KmRuns();
+
+    @Query(value = "SELECT *  from run t  WHERE t.distance = 56.50 limit 5",
+            nativeQuery = true)
+    List<Triathlon> findLast5650KmRuns();
+
+    @Query(value = "SELECT *  from run t  WHERE t.distance = 112.99 limit 5",
+            nativeQuery = true)
+    List<Triathlon> findLast11299KmRuns();
+
 }
 
