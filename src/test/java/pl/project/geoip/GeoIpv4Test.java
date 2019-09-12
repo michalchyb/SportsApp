b2d0b5a94c4a1df0c362a6aeba721d3d809ece93
Michał Chyb <michalchyb1@gmail.com>
2019-09-12 20:08:14 +0200

    refactor

--- src/test/java/pl/project/geoip/GeoIpv4Test.java
+++ src/test/java/pl/project/geoip/GeoIpv4Test.java
@@ -4,19 +4,21 @@ import org.junit.Test;
 import pl.project.commons.GeoHelpers;
 import pl.project.commons.geoip.GeoIPv4;
 
-import java.net.UnknownHostException;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class GeoIpv4Test {
 
+    private static final String CITY_POZNAN = "Poznan";
+    private static final String CITY_NEW_YORK = "New York";
+
     @Test
-    public void test_001() throws UnknownHostException {
-        assertEquals("Poznan", GeoIPv4.getLocation(GeoHelpers.getHostIpAddress()).getCity());
+    public void test_001() {
+        assertEquals(CITY_POZNAN, GeoIPv4.getLocation(GeoHelpers.getHostIpAddress()).getCity());
     }
 
     @Test
     public void test_002() {
-        assertEquals("New York", GeoIPv4.getLocation("72.229.28.185").getCity());
+        assertEquals(CITY_NEW_YORK, GeoIPv4.getLocation("72.229.28.185").getCity());
     }
+
 }
