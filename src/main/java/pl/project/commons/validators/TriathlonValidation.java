a54d52b80eb41778b5315bb8fae0aa734d1002e2
Michał Chyb <michalchyb1@gmail.com>
2019-09-13 18:42:34 +0200

    remove validators

--- src/main/java/pl/project/commons/validators/TriathlonValidation.java
+++
@@ -1,6 +0,0 @@
-package pl.project.commons.validators;
-
-
-public class TriathlonValidation {
-    //TODO must add common validations  for triathlon fields
-}
--- src/main/java/pl/project/commons/validators/Validation.java
+++
@@ -1,9 +0,0 @@
-package pl.project.commons.validators;
-
-
-import org.springframework.validation.Validator;
-
-public abstract class Validation implements Validator {
-}
-
-
