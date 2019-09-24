ddc35ec8977f6c983cc598aa6e65f227a87da94f
Michał Chyb <michalchyb1@gmail.com>
2019-09-24 17:48:43 +0200

    handling directory for photos

--- src/main/java/pl/project/commons/storage/LocalFileService.java
+++ src/main/java/pl/project/commons/storage/LocalFileService.java
@@ -3,6 +3,11 @@ package pl.project.commons.storage;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Service
@@ -14,5 +19,22 @@ public class LocalFileService {
 
     public LocalFileService(ServletContext servletContext) {
         this.servletContext = servletContext;
+        createDirectory();
     }
+
+    private void createDirectory() {
+        uploads = servletContext.getRealPath("/uploads/");
+        log.log(Level.INFO, uploads);
+
+        Path path = Paths.get(uploads);
+        if (!Files.exists(path)) {
+            try {
+                Files.createDirectory(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
