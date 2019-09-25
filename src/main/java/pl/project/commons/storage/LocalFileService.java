f397a1bc10f9e2b6289c3f1c94c994d3aa17fee4
Michał Chyb <michalchyb1@gmail.com>
2019-09-25 20:02:33 +0200

    get lis of files from directory method

--- src/main/java/pl/project/commons/storage/LocalFileService.java
+++ src/main/java/pl/project/commons/storage/LocalFileService.java
@@ -1,14 +1,18 @@
 package pl.project.commons.storage;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.ServletContext;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @Service
 public class LocalFileService {
@@ -36,5 +40,41 @@ public class LocalFileService {
         }
     }
 
+    public List<LocalFile> getFilesFromDirector() throws IOException {
 
+        return Files.walk(Paths.get(uploads))
+                .filter(Files::isRegularFile)
+                .map(f -> {
+                    try {
+                        BasicFileAttributes basicFileAttributes = Files.
+                                readAttributes(f.toAbsolutePath(), BasicFileAttributes.class);
+                        String downloadUri = ServletUriComponentsBuilder
+                                .fromCurrentContextPath()
+                                .path("/api/files/download")
+                                .path(f.getFileName().toString())
+                                .toUriString();
+
+                        String deleteUri = ServletUriComponentsBuilder
+                                .fromCurrentContextPath()
+                                .path("/api/files/delete")
+                                .path(f.getFileName().toString())
+                                .toUriString();
+
+                        return LocalFile.builder()
+                                .name(f.getFileName().toString())
+                                .creationTime(basicFileAttributes.creationTime().toString())
+                                .lastModified(basicFileAttributes.lastModifiedTime().toString())
+                                .size(basicFileAttributes.size())
+                                .downloadUri(downloadUri)
+                                .deleteUri(deleteUri)
+                                .fileType(Files.probeContentType(f.toAbsolutePath()))
+                                .build();
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }).collect(Collectors.toList());
+    }
 }
