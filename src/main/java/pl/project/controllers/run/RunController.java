d72056a5aed2de028bf912134095549726227941
Michał Chyb <michalchyb1@gmail.com>
2020-01-20 17:16:26 +0100

    rollback to one RunController

--- src/main/java/pl/project/controllers/run/RunController.java
+++ src/main/java/pl/project/controllers/run/RunController.java
@@ -78,6 +78,21 @@ public class RunController {
         return runService.getRunsDTO();
     }
 
+    @GetMapping("dto/runs/5km")
+    public List<RunDTO> getLast5KmRuns() {
+        return runService.getLast5KmRuns();
+    }
+
+    @GetMapping("dto/runs/10km")
+    public List<RunDTO> getLast10KmRuns() {
+        return runService.getLast10KmRuns();
+    }
+
+    @GetMapping("dto/runs/21km")
+    public List<RunDTO> getLast21mRuns() {
+        return runService.getLast21KmRuns();
+    }
+
     @GetMapping("runs/file")
     public String createFile() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException {
         CreatorXLS<RunDTO> creatorXLS = new CreatorXLS<>(RunDTO.class);
