39084a8c3457ca92cfec36c1673bff73cd3666f3
Michał Chyb <michalchyb1@gmail.com>
2020-01-25 14:19:28 +0100

    add User and LoginApi

---
+++ src/main/java/pl/project/commons/securities/controllers/LoginApi.java
@@ -0,0 +1,15 @@
+package pl.project.commons.securities.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.project.commons.securities.models.User;
+
+@RestController
+public class LoginApi {
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        return "";
+    }
+}
