66ebc57eedf44542ce503789b6ec85f244a82e21
Michał Chyb <michalchyb1@gmail.com>
2020-01-27 19:58:16 +0100

    # simple token impl WIP

--- src/main/java/pl/project/commons/securities/filter/JwtFilter.java
+++ src/main/java/pl/project/commons/securities/filter/JwtFilter.java
@@ -1,14 +1,27 @@
 package pl.project.commons.securities.filter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class JwtFilter implements javax.servlet.Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        ((HttpServletRequest) servletRequest).getHeader("Authorization");
+        if (httpServletRequest == null || !((HttpServletRequest) servletRequest).getHeader("Authorization").startsWith("Bearer_")) {
+            throw new ServletException("Wrong or empty header");
+        } else {
+            String token = ((HttpServletRequest) servletRequest).getHeader("Authorization").substring(7);
+            Claims claims = Jwts.parser().setSigningKey("michal123").parseClaimsJws(token).getBody();
+        }
 
     }
 }
