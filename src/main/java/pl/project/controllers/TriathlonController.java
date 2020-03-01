4f07074bd4acaea169923106bf68dd39e09aeba5
Michał Chyb <michalchyb1@gmail.com>
2020-03-01 09:09:09 +0100

    # controller refactor

--- src/main/java/pl/project/controllers/TriathlonController.java
+++ src/main/java/pl/project/controllers/TriathlonController.java
@@ -1,17 +1,10 @@
 package pl.project.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import pl.project.commons.extras.CreatorXLS;
-import pl.project.models.Triathlon;
-import pl.project.models.dtos.TriathlonDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.project.repositories.TriathlonRepository;
 import pl.project.services.TriathlonService;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -26,69 +19,4 @@ public class TriathlonController {
         this.triathlonService = triathlonService;
     }
 
-    @GetMapping("triathlons")
-    public ResponseEntity<List<Triathlon>> getTriathlons() {
-        return new ResponseEntity<>(triathlonRepository.findAll(), HttpStatus.OK);
-    }
-
-    @PostMapping("triathlons")
-    public ResponseEntity<Triathlon> addTriathlon(@RequestBody Triathlon triathlon) {
-        return new ResponseEntity<>(triathlonRepository.save(triathlon), HttpStatus.OK);
-    }
-
-    @RequestMapping("triathlons/findById/{triathlonId}")
-    public ResponseEntity<Triathlon> findById(@PathVariable long triathlonId) {
-        return ResponseEntity.ok(triathlonRepository.findById(triathlonId));
-    }
-
-    @RequestMapping("triathlons/delete/{triathlonId}")
-    public ResponseEntity<Triathlon> deleteById(@PathVariable long triathlonId) {
-        triathlonRepository.deleteById(triathlonId);
-        return ResponseEntity.noContent().build();
-    }
-
-    @PostMapping("dto/triathlons")
-    public Triathlon addTriathlon(@RequestBody TriathlonDTO triathlonDTO) {
-        return triathlonService.addTriathlon(triathlonDTO);
-    }
-
-    @PutMapping("dto/triathlons")
-    public void updateTriathlon(@RequestBody TriathlonDTO triathlonDTO) {
-        triathlonService.updateTriathlon(triathlonDTO);
-    }
-
-    @DeleteMapping("dto/triathlons{nameRun}")
-    public void deleteTriathlon(@PathVariable String nameRun) {
-        triathlonService.deleteTriathlon(nameRun);
-    }
-
-    @GetMapping("dto/triathlons")
-    public List<TriathlonDTO> getTriathlonsDTO(@RequestParam(value = "distance", required = false) Double distance) {
-        if (distance != null && distance > 0) {
-            return triathlonService.getTriathlonsByDistance(distance);
-        }
-        return triathlonService.getTriathlonsDTO();
-    }
-
-    @GetMapping("dto/triathlons/2825")
-    public List<TriathlonDTO> getLast2825KmRuns() {
-        return triathlonService.getLast2825KmTriathlons();
-    }
-
-    @GetMapping("dto/triathlons/5650")
-    public List<TriathlonDTO> getLast5650KmRuns() {
-        return triathlonService.getLast5650mTriathlons();
-    }
-
-    @GetMapping("dto/triathlons/11299")
-    public List<TriathlonDTO> getLast11299mRuns() {
-        return triathlonService.getLast11299mTriathlons();
-    }
-
-    @GetMapping("triathlons/file")
-    public String createFile() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException {
-        CreatorXLS<TriathlonDTO> creatorXLS = new CreatorXLS<>(TriathlonDTO.class);
-        creatorXLS.createFile(triathlonService.getTriathlonsDTO(), "src/main/", "triathlons");
-        return "test";
-    }
 }
--- src/main/java/pl/project/controllers/run/RunController.java
+++ src/main/java/pl/project/controllers/run/RunController.java
@@ -1,20 +1,10 @@
 package pl.project.controllers.run;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-import pl.project.commons.extras.CreatorXLS;
-import pl.project.commons.validators.RunValidator;
-import pl.project.models.Run;
-import pl.project.models.dtos.RunDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.project.repositories.RunRepository;
 import pl.project.services.RunService;
-
-import javax.validation.Valid;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -29,74 +19,5 @@ public class RunController {
         this.runService = runService;
     }
 
-    @GetMapping("runs")
-    public ResponseEntity<List<Run>> getRuns() {
-        return runService.getRuns();
-    }
 
-    @InitBinder("runDto")
-    protected void initBinder(WebDataBinder binder) {
-        binder.addValidators(new RunValidator());
-    }
-
-    @PostMapping("runs")
-    public ResponseEntity addRun(@RequestBody @Valid Run run) {
-        return new ResponseEntity<>(runRepository.save(run), HttpStatus.OK);
-    }
-
-    @PostMapping("dto/runs")
-    public Run addRun(@RequestBody @Valid RunDTO runDTO) {
-        return runService.addRun(runDTO);
-    }
-
-    @PutMapping("dto/runs")
-    public void updateRun(@RequestBody RunDTO runDTO) {
-        runService.updateRun(runDTO);
-    }
-
-    @DeleteMapping("dto/runs{nameRun}")
-    public void deleteRun(@PathVariable String nameRun) {
-        runService.deleteRun(nameRun);
-    }
-
-    @RequestMapping("runs/findById/{runId}")
-    public ResponseEntity<Run> findById(@PathVariable long runId) {
-        return ResponseEntity.ok(runRepository.findById(runId));
-    }
-
-    @RequestMapping("runs/delete/{runId}")
-    public ResponseEntity<Run> deleteById(@PathVariable long runId) {
-        runRepository.deleteById(runId);
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("dto/runs")
-    public List<RunDTO> getRunsDTO(@RequestParam(value = "distance", required = false) Double distance) {
-        if (distance != null && distance > 0) {
-            return runService.getRunsByDistance(distance);
-        }
-        return runService.getRunsDTO();
-    }
-
-    @GetMapping("dto/runs/5km")
-    public List<RunDTO> getLast5KmRuns() {
-        return runService.getLast5KmRuns();
-    }
-
-    @GetMapping("dto/runs/10km")
-    public List<RunDTO> getLast10KmRuns() {
-        return runService.getLast10KmRuns();
-    }
-
-    @GetMapping("dto/runs/21km")
-    public List<RunDTO> getLast21mRuns() {
-        return runService.getLast21KmRuns();
-    }
-
-    @GetMapping("runs/file")
-    public String createFile() throws NoSuchMethodException, IOException, IllegalAccessException, InvocationTargetException {
-        CreatorXLS<RunDTO> creatorXLS = new CreatorXLS<>(RunDTO.class);
-        creatorXLS.createFile(runService.getRunsDTO(), "src/main/", "runs");
-        return "test";
-    }
 }
