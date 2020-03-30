0a380215ce0e93b949be18d1ebdec1a99b147baf
Michał Chyb <michalchyb1@gmail.com>
2020-03-30 19:10:53 +0200

    add controller

---
+++ src/main/java/pl/project/controller/GenericController.java
@@ -0,0 +1,13 @@
+package pl.project.controller;
+
+import java.io.IOException;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/")
+public class GenericController {
+
+
+    public GenericController() {
+    }
+}
