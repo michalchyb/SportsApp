662f17e4e8b2731b3ed0b0962ddb8e56cea7fb04
Michał Chyb <michalchyb1@gmail.com>
2020-04-03 16:32:44 +0200

    # add getWeatherDto functionality

--- src/main/java/pl/project/controller/GenericController.java
+++ src/main/java/pl/project/controller/GenericController.java
@@ -1,6 +1,8 @@
 package pl.project.controller;
 
-import java.io.IOException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
--- src/main/java/pl/project/controller/HomeController.java
+++ src/main/java/pl/project/controller/HomeController.java
@@ -1,44 +1,14 @@
 package pl.project.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.maxmind.geoip2.exception.GeoIp2Exception;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.project.common.WeatherHelpers;
-import pl.project.model.geo.GeoLocalization;
-import pl.project.model.weather.WeatherObject;
-import pl.project.service.GeoLocalizationService;
-import pl.project.service.WeatherService;
-
-import java.io.IOException;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/")
 public class HomeController {
 
-    private GeoLocalizationService geoLocalization;
-    private WeatherService weatherService;
-
-    public HomeController(GeoLocalizationService geoLocalization, WeatherService weatherService) {
-        this.geoLocalization = geoLocalization;
-        this.weatherService = weatherService;
-    }
-
-    @GetMapping("location")
-    public GeoLocalization getLocation() throws IOException, GeoIp2Exception {
-        return geoLocalization.getLocation();
-    }
-
-    @GetMapping("weather")
-    public StringBuilder getWeatherForFoundLocalization() throws IOException {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String weatherOutput = weatherService.getWeatherFromApi();
-        WeatherObject weatherObject = objectMapper.readValue(weatherOutput, WeatherObject.class);
-
-        return WeatherHelpers.printWeatherForLocation(weatherObject);
+    public HomeController() {
     }
 }
