bc0e0fad1b54475fc7582fb8ee1dc4df43c8a214
Michał Chyb <michalchyb1@gmail.com>
2020-05-04 13:12:05 +0200

    # round temp values for frontend # add dependency decimal4j

--- src/main/java/pl/project/weather/mapper/WeatherMapper.java
+++ src/main/java/pl/project/weather/mapper/WeatherMapper.java
@@ -1,5 +1,6 @@
 package pl.project.weather.mapper;
 
+import org.decimal4j.util.DoubleRounder;
 import org.springframework.stereotype.Component;
 import pl.project.weather.model.dto.WeatherDto;
 import pl.project.weather.model.weather.WeatherObject;
@@ -12,10 +13,10 @@ public class WeatherMapper {
                 .name(from.getName())
                 .weather(from.getWeather().get(0).getMain())
                 .weatherIcon(from.getWeather().get(0).getIcon())
-                .temp(from.getMain().getTemp())
+                .temp(DoubleRounder.round(from.getMain().getTemp(), 1))
                 .pressure(from.getMain().getPressure())
-                .minTemp(from.getMain().getTempMin())
-                .maxTemp(from.getMain().getTempMax())
+                .minTemp(DoubleRounder.round(from.getMain().getTempMin(), 1))
+                .maxTemp(DoubleRounder.round(from.getMain().getTempMax(), 1))
                 .build();
     }
 }
