a3a0d44d626f339df42ba8c7d43b8871cf463ed1
Michał Chyb <michalchyb1@gmail.com>
2020-05-14 18:24:14 +0200

    Revert "refactor commit"
    This reverts commit 1f687a45

--- src/main/java/pl/project/security/config/WebSecurityConfig.java
+++ src/main/java/pl/project/security/config/WebSecurityConfig.java
@@ -12,11 +12,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-//        http.authorizeRequests()
-//                .antMatchers("/test2").authenticated()// zalogowany
-//                .antMatchers("/test3").hasRole("ADMIN")
-//                .and()
-//                .addFilter(new JwtFilter(authenticationManager()));
+        http.authorizeRequests()
+                .antMatchers("/test2").authenticated()// zalogowany
+                .antMatchers("/test3").hasRole("ADMIN")
+                .and()
+                .addFilter(new JwtFilter(authenticationManager()));
         http.csrf().disable();
         http.cors();
     }
