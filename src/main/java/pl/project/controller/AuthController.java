4aff6a518dcc5c91b158cafdbff2369cebdc47fa
Michał Chyb <michalchyb1@gmail.com>
2020-07-22 17:09:06 +0200

    # add login

--- src/main/java/pl/project/controller/AuthController.java
+++ src/main/java/pl/project/controller/AuthController.java
@@ -2,19 +2,29 @@ package pl.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pl.project.model.ERole;
+import pl.project.model.LoginRequest;
 import pl.project.model.Role;
 import pl.project.model.User;
 import pl.project.payload.request.SignupRequest;
+import pl.project.payload.response.JwtResponse;
 import pl.project.payload.response.MessageResponse;
 import pl.project.repository.RoleRepository;
 import pl.project.repository.UserRepository;
+import pl.project.security.jwt.JwtUtils;
+import pl.project.security.services.UserDetailsImpl;
 
 import javax.validation.Valid;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -28,6 +38,12 @@ public class AuthController {
 
     @Autowired
     private PasswordEncoder encoder;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
@@ -77,5 +93,25 @@ public class AuthController {
 
         return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
 
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
     }
 }
--- src/main/java/pl/project/repository/UserRepository.java
+++ src/main/java/pl/project/repository/UserRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.project.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    Optional<User> findByUserName(String userName);
+    Optional<User> findByUsername(String userName);
 
     Boolean existsByUsername(String username);
 
